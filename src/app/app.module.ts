5726aeb48dd66a3d19c709eea01985fcd2a820c0
Joe Spalding <boloneyshinobi@gmail.com>
2018-10-01 08:48:13 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PersonalcontactinfoComponent } from './components/shared/personalcontactinfo/personalcontactinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
+    ResumeComponent,
+    ContactComponent,
+    PersonalcontactinfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
